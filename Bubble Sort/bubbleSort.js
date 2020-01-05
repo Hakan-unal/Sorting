add3748981fd69858d20deb41fd3c390fab85f6e
Hakan-unal <hakanunal_1996@hotmail.com>
2020-01-06 02:01:52 +0300

    first commit

---
+++ Bubble Sort/bubbleSort.js
@@ -0,0 +1,19 @@
+let input, send, output, display,sort;
+
+input = document.querySelector("#input");
+send = document.querySelector("#send");
+display = document.querySelector("#display");
+output = document.querySelector("#output");
+sort=document.querySelector("#sort")
+
+
+
+
+
+
+
+console.log(input, send, display, output,sort);
+
+
+
+
