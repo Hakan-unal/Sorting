c74f860655b084ff0e779376be65367fb4b7f217
Hakan-unal <hakanunal_1996@hotmail.com>
2020-01-07 00:05:20 +0300

    code updated

--- Selection Sort/selectionSort.js
+++ Selection Sort/selectionSort.js
@@ -1,4 +1,4 @@
-let input, output, display, send, sort, array;
+let input, output, display, send, sort, array, selectionSort;
 
 input = document.querySelector("#input");
 output = document.querySelector("#output");
@@ -8,15 +8,42 @@ sort = document.querySelector("#sort");
 
 array = [];
 
+
+selectionSort = (arr) => {
+    let count
+    count = arr.length;
+    for (let i = 0; i < count; i++) {
+        let min;
+        min = i;
+        for (let j = i + 1; j < count; j++) {
+            if (arr[min] > arr[j]) {
+                min = j;
+            }
+        }
+        if (min !== i) {
+            let temp;
+            temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+    return arr;
+}
+
 send.addEventListener("click", () => {
     let val;
     val = Number(input.value);
     array.push(val);
     display.value = array.join(", ");
     input.value = "";
-
 });
 
+
+sort.addEventListener("click", () => {
+    let sum;
+    sum = selectionSort(array);
+    output.value = sum.join(", ");
+})
 
 
 
