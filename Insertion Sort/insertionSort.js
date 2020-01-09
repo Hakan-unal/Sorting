e5b18b289cee54a6f582b32a30c6a60be8463f2a
Hakan-unal <hakanunal_1996@hotmail.com>
2020-01-09 22:07:10 +0300

    code updated

---
+++ Insertion Sort/insertionSort.js
@@ -0,0 +1,7 @@
+let input, array, display, send, sort;
+
+input = document.querySelector("#input");
+array = document.querySelector("#array");
+display = document.querySelector("#display");
+send = document.querySelector("#send");
+sort = document.querySelector("#sort");
